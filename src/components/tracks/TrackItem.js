ee82f33ffd9b0bbf6bdd4696cd1feaa2d5194d91
Dániel Agg <daniel.agg@outlook.com>
2019-04-28 15:33:47 +0200

    Styling TrackItem component

--- src/components/tracks/TrackItem.js
+++ src/components/tracks/TrackItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const TrackItem = props => {
   const { track } = props;
@@ -7,7 +8,27 @@ const TrackItem = props => {
     <div className="col-md-6">
       <div className="card mb-4 shadow-sm">
         <div className="card-body">
-          <p>{track.artist_name}</p>
+          <h5 className="mb-3">{track.artist_name}</h5>
+          <p className="card-text">
+            <strong>
+              <i className="fas fa-play mr-2" />
+              Track
+            </strong>
+            : {track.track_name}
+            <br />
+            <strong>
+              <i className="fas fa-compact-disc mr-2" />
+              Album
+            </strong>
+            : {track.album_name}
+          </p>
+          <Link
+            to={`lyrics/track/${track.track_id}`}
+            className="btn btn-dark btn-block"
+          >
+            <i className="fas fa-chevron-right mr-2" />
+            View Lyrics
+          </Link>
         </div>
       </div>
     </div>
